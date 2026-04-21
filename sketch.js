990f87368bb2c42bf01846b3fb30a2cb555f174f
juliachiao <julia.jih.0507@gmail.com>
2026-04-21 10:27:06 +0800

    1.01_修改攝影機左右顛倒問題

--- sketch.js
+++ sketch.js
@@ -1,40 +1,38 @@
 let capture;
 
 function setup() {
-  // 第一步驟：產生一個全螢幕的畫布
+  // 1. 產生一個全螢幕的畫布
   createCanvas(windowWidth, windowHeight);
-
-  // 取得攝影機影像
+  
+  // 2. 擷取攝影機影像
   capture = createCapture(VIDEO);
   
-  // 隱藏預設產生的 HTML 影片元素，這樣我們才能純粹將影像繪製在畫布上
-  capture.hide(); 
+  // 隱藏預設產生的 HTML 攝影機元件，我們只需要在畫布上繪製它
+  capture.hide();
 }
 
 function draw() {
-  // 設定畫布背景顏色為 cdb4db
+  // 3. 設定背景顏色為 cdb4db
   background('#cdb4db');
 
-  // 設定顯示的影像寬度與高度為整個畫布寬高的 50%
-  let imgWidth = width * 0.5;
-  let imgHeight = height * 0.5;
+  // 4. 計算影像的大小 (寬度與高度皆為畫布的 50%)
+  let imgW = width * 0.5;
+  let imgH = height * 0.5;
 
-  // 計算讓影像置中於畫布的 X 與 Y 座標
-  let x = (width - imgWidth) / 2;
-  let y = (height - imgHeight) / 2;
+  // 5. 計算置中座標
+  // 畫布寬度的一半減去影像寬度的一半，即可得到置中的 X 座標
+  let x = (width - imgW) / 2;
+  let y = (height - imgH) / 2;
 
-  // 將攝影機的即時影像繪製在畫布中間
-  image(capture, x, y, imgWidth, imgHeight);
+  // 6. 將攝影機影像繪製在畫布中間 (使用 scale 進行水平翻轉以修正左右顛倒)
+  push();
+  translate(width, 0); // 將座標原點移至畫布右側
+  scale(-1, 1);        // 水平翻轉畫布 X 軸
+  image(capture, x, y, imgW, imgH);
+  pop(); // 恢復原本的畫布設定
 }
 
-// 當瀏覽器視窗大小改變時，自動調整畫布尺寸以維持全螢幕
+// 當視窗大小改變時，自動調整畫布大小以維持全螢幕
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
 }
